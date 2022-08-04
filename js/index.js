d999ef768669cfc84b543d4da23d108702c6cf2e
SamanthaDiazWebDev <samantha.diaz1019@gmail.com>
2022-08-03 23:30:54 -0400

    Added a form with HTML and JS

--- js/index.js
+++ js/index.js
@@ -16,5 +16,35 @@ for (let i = 0; i < skills.length; i++) {
     skillsList.appendChild(skill);
 }
 
+const messageForm = document.getElementsByName("leave_message");
+const messageSection = document.getElementById('messages');
+messageSection.hidden = true;
+
+messageForm.item(0).addEventListener('submit', (event) => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const message = event.target.message.value;
+
+    const messageList = messageSection.querySelector('ul');
+    const newMessage = document.createElement('li');
+    newMessage.innerHTML = `<a href="mailto: ${email}">${name}</a> wrote: <span>${message}</span>`;
+    
+    const removeButton = document.createElement('button');
+    removeButton.innerText = 'remove';
+    removeButton.type = 'button';
+    removeButton.addEventListener('click', () => {
+        const entry = removeButton.parentNode;
+        entry.remove();
+    });
+
+    messageList.appendChild(newMessage);
+    newMessage.appendChild(removeButton);
+    messageSection.hidden = false;
+    messageForm.item(0).reset();
+
+   
+});
+
 //const title = document.querySelector('h1');
 //title.style.color = '#ADD8E6';
